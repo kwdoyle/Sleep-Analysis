0b11dc472d3575de6d27ac0d1060c935d6072e5c
kwdoyle <cheeze13579@gmail.com>
2015-06-27 18:32:29 -0400

    initial commit

---
+++ Sleep Quality.R
@@ -0,0 +1,29 @@
+sleep <- read.csv("sleepdata.csv", sep=";")
+
+# make sleep quality column not a factor anymore
+sleep[,3] <- as.character(sleep[,3])
+
+# remove % symbol from data
+sleep[,3] <- sub("%", "", sleep[,3])
+
+# remove outlier steps data point
+sleep <- sleep[-81,]
+
+# fit linear model
+steps <- sleep[,8]
+quality <- as.numeric(sleep[,3])
+
+fit <- lm(quality ~ steps)
+B0 <- coef(fit)[1]  # intercept
+B1 <- coef(fit)[2]  # slope
+
+summary(fit)
+
+# plot % sleep quality vs number of steps
+plot(sleep[,8], sleep[,3], xlab="steps", ylab="% sleep quality",
+     main="Effect of activity on \n sleep quality ", pch=16)
+# add regression line
+abline(a=B0, b=B1)
+
+# test for correlation
+cor.test(quality, steps)
